94071f3c2d3073c8b2e1844c988eccbfbd92ac76
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-27 16:56:55 +0400

    fake projects and tasks added

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -27,8 +27,10 @@ class TaskFactory extends Factory
 
             'image_path' => fake()->imageUrl(),
             'assigned_user_id' => 1,
-            'created_at' => 1,
-            'updated_at' => 1
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
         ];
     }
 }
