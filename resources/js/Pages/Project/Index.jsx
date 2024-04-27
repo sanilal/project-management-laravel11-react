47f34745d21a97368f71f2ce7ea465fb1aa5f63c
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-27 18:58:41 +0400

    Projects listed

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({auth, projects}) {
   return (
@@ -13,7 +13,42 @@ export default function Index({auth, projects}) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <pre>{JSON.stringify(projects, undefined, 2)}</pre>
+                            <div className="p-6 text-gray-900 dark:text-gray-100">
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                        <tr className="text-nowrap">
+                                            <th className="px-3 py-2">ID</th>
+                                            <th className="px-3 py-2">Image</th>
+                                            <th className="px-3 py-2">Name</th>
+                                            <th className="px-3 py-2">Status</th>
+                                            <th className="px-3 py-2">Created Date</th>
+                                            <th className="px-3 py-2">Due Date</th>
+                                            <th className="px-3 py-2">Created By</th>
+                                            <th className="px-3 py-2 text-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {projects.data.map((project)=>(
+                                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td className="px-3 py2 ">{project.id}</td>
+                                            <td className="px-3 py2 ">
+                                                <img src={project.image_path} alt="projet image" className="w-32 h-32" />
+                                            </td>
+                                            <td className="px-3 py2 ">{project.name}</td>
+                                            <td className="px-3 py2 ">{project.status}</td>
+                                            <td className="px-3 py2 text-nowrap">{project.created_at}</td>
+                                            <td className="px-3 py2 text-nowrap">{project.due_date}</td>
+                                            <td className="px-3 py2 ">{project.createdBy.name}</td>
+                                            <td className="px-3 py2 ">
+                                                <Link href={route('project.edit', project.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">Edit</Link>    
+                                                <Link href={route('project.destroy', project.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">Delete</Link>    
+                                            </td>
+                                            
+                                        </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
